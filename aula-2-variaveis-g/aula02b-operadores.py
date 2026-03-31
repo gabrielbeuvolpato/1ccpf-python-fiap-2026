6673844ca65b790958fd8c8425739d8800dfe724
gabrielbeuvolpato <gabrielbeuvolpato@gmail.com>
2026-03-31 11:41:48 -0300

    aula 03

---
+++ aula-2-variaveis-g/aula02b-operadores.py
@@ -0,0 +1,39 @@
+num1 = 5
+num2 = 2
+
+print(type(num1), type(num2))
+
+resultado = num1 + num2
+print(resultado, type(resultado))
+
+#OPERADORES DE ATRIBUIÇAO
+
+num = 15
+print()
+print(num)
+
+num = num + 2
+print(num)
+
+num += 2
+print(num)
+
+#OPERADORES RELACIONAIS
+print()
+print(6 == 2)
+
+idade = 18
+print(idade == 10)
+
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade, type(maior_idade))
+
+#STRINGS
+nome = "Marcos"
+nome2 = "marcos"
+
+print(nome.upper() == nome2.upper())
---
+++ aula-3-modulos/aula3c-condicionais.py
@@ -0,0 +1,13 @@
+#CONDICIONAIS
+
+nota_final = 6
+
+if nota_final < 4:
+    print('Reprovado')
+
+elif nota_final < 6:
+    print("Recuperacao")
+else:
+    print("Aprovado")
+
+print('FIM')
